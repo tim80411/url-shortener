cf5429b91f522891cfb13d2e7c5f15f88d73dbb1
tim80411 <tim80411@gmail.com>
2021-05-18 23:54:52 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 
 const app = express()
 
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
 app.get('/', (req, res) => {
-  res.send('success')
+  res.render('index')
 })
-
-
 
 app.listen(3000, () => {
   console.log('this app is running on http://localhost:3000')
