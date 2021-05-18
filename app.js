f8b9aa4501d0fc6229eddb3d79918c80bdd122e8
tim80411 <tim80411@gmail.com>
2021-05-18 23:47:28 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('success')
+})
+
+
+
+app.listen(3000, () => {
+  console.log('this app is running on http://localhost:3000')
+})
