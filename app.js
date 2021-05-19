34d103ec21c172f2c7440a0c8213a5e2a9b280e8
tim80411 <tim80411@gmail.com>
2021-05-19 14:44:40 +0800

    feat: add random url generater function

--- app.js
+++ app.js
@@ -1,15 +1,65 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const Url = require('./models/url.js')
+const helpers = require('./config/helpers')
+const bodyParser = require('body-parser')
+const myFunctions = require('./config/myFunctions')
+
+const DOMAIN = 'http://localhost:3000'
 
 const app = express()
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+require('./config/mongoose')
+
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main',
+  helpers
+}))
 app.set('view engine', 'handlebars')
 
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
+
   res.render('index')
+})
+
+app.post('/', (req, res) => {
+  const redirectUrl = req.body.url
+
+  let shortenUrl = myFunctions.generateRandom()
+  return Url.find()
+    .lean()
+    .then(urls => {
+      while (urls.some(url => url.shortenUrl === shortenUrl)) {
+        shortenUrl = myFunctions.generateRandom()
+      }
+      return Url.create({ redirectUrl, shortenUrl })
+        .then(() => {
+          shortenUrl = `${DOMAIN}/${shortenUrl}`
+          res.render('finish', { shortenUrl })
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
+
+app.get('/:url', (req, res) => {
+  const shortenUrl = req.params.url
+
+  return Url.findOne({ shortenUrl })
+    .lean()
+    .then(url => {
+      res.redirect(url.redirectUrl)
+    })
 })
 
 app.listen(3000, () => {
   console.log('this app is running on http://localhost:3000')
 })
+
---
+++ config/myFunctions.js
@@ -0,0 +1,23 @@
+const library = {
+  generateRandom: (length = 5) => {
+    let random = ''
+
+    const lowerLetters = 'abcdefghijklmnopqrstuvwxyz'
+    const upperLetters = lowerLetters.toUpperCase()
+    const nums = '0123456789'
+
+    const randomPool = random.concat(...lowerLetters, ...upperLetters, ...nums)
+
+
+    for (i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * randomPool.length)
+
+      random += randomPool[randomIndex]
+    }
+
+    return random
+  }
+}
+
+
+module.exports = library
